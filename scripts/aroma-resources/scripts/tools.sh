6bedb74cbb0bf0d4e8b9733fa4221b1e3aded2be
Lutz Schwachhöfer <shark@shark5060.net>
2015-10-14 20:46:06 +0200

    Update tools.sh

--- scripts/aroma-resources/scripts/tools.sh
+++ scripts/aroma-resources/scripts/tools.sh
@@ -6,21 +6,21 @@
 
 case "$1" in
 	save)
-	rm -f /sdcard1/Shark-GApps/aromagapps.prop
-	rm -f /sdcard1/Shark-GApps/bypass.prop
-	rm -f /sdcard1/Shark-GApps/rem.prop
-	rm -f /sdcard1/Shark-GApps/extra.prop
-	[ ! -d /sdcard1/Shark-GApps ] && mkdir /sdcard1/Shark-GApps
-	cp /tmp/aroma/aromagapps.prop /sdcard1/Shark-GApps/aromagapps.prop
-	cp /tmp/aroma/bypass.prop /sdcard1/Shark-GApps/bypass.prop
-	cp /tmp/aroma/rem.prop /sdcard1/Shark-GApps/rem.prop
-	cp /tmp/aroma/extra.prop /sdcard1/Shark-GApps/extra.prop
+	rm -f /external_sd/Shark-GApps/aromagapps.prop
+	rm -f /external_sd/Shark-GApps/bypass.prop
+	rm -f /external_sd/Shark-GApps/rem.prop
+	rm -f /external_sd/Shark-GApps/extra.prop
+	[ ! -d /external_sd/Shark-GApps ] && mkdir /external_sd/Shark-GApps
+	cp /tmp/aroma/aromagapps.prop /external_sd/Shark-GApps/aromagapps.prop
+	cp /tmp/aroma/bypass.prop /external_sd/Shark-GApps/bypass.prop
+	cp /tmp/aroma/rem.prop /external_sd/Shark-GApps/rem.prop
+	cp /tmp/aroma/extra.prop /external_sd/Shark-GApps/extra.prop
 	;;
 	load)
-	[ -f /sdcard1/Shark-GApps/aromagapps.prop ] && rm -f /tmp/aroma/aromagapps.prop && cp /sdcard1/Shark-GApps/aromagapps.prop /tmp/aroma/aromagapps.prop
-	[ -f /sdcard1/Shark-GApps/bypass.prop ] && rm -f /tmp/aroma/bypass.prop && cp /sdcard1/Shark-GApps/bypass.prop /tmp/aroma/bypass.prop
-	[ -f /sdcard1/Shark-GApps/rem.prop ] && rm -f /tmp/aroma/rem.prop && cp /sdcard1/Shark-GApps/rem.prop /tmp/aroma/rem.prop
-	[ -f /sdcard1/Shark-GApps/extra.prop ] && rm -f /tmp/aroma/extra.prop && cp /sdcard1/Shark-GApps/extra.prop /tmp/aroma/extra.prop
+	[ -f /external_sd/Shark-GApps/aromagapps.prop ] && rm -f /tmp/aroma/aromagapps.prop && cp /external_sd/Shark-GApps/aromagapps.prop /tmp/aroma/aromagapps.prop
+	[ -f /external_sd/Shark-GApps/bypass.prop ] && rm -f /tmp/aroma/bypass.prop && cp /external_sd/Shark-GApps/bypass.prop /tmp/aroma/bypass.prop
+	[ -f /external_sd/Shark-GApps/rem.prop ] && rm -f /tmp/aroma/rem.prop && cp /external_sd/Shark-GApps/rem.prop /tmp/aroma/rem.prop
+	[ -f /external_sd/Shark-GApps/extra.prop ] && rm -f /tmp/aroma/extra.prop && cp /external_sd/Shark-GApps/extra.prop /tmp/aroma/extra.prop
 	;;
 	reset)
 	rm -f /tmp/aroma/aromagapps.prop
